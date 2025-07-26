17c8598bab33fd749001307c187d4cb1629d7286
Arash <arash8877@gmail.com>
2025-07-26 16:38:47 +0200

    create Hero file

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {}, // instead of 'tailwindcss'
     autoprefixer: {},
   },
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-
-const Hero = () => {
+export default function Hero() {
   return (
-    <div>Hero</div>
-  )
+    <section className="relative border-2 w-screen h-screen">
+      <div className="absolute -bottom-44 left-0 w-full max-w-[1300px] sm:max-w-[700px] md:max-w-[900px] lg:max-w-[1100px] xl:max-w-[1300px]">
+        <img src="/three-circles-with-flowers.svg" alt="three-circles" className="w-full h-auto" />
+      </div>
+    </section>
+  );
 }
-
-export default Hero
