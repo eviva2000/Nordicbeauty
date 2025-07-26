f7e0908314a9b14e404c28533857fe13167b929e
Arash <arash8877@gmail.com>
2025-07-26 15:30:03 +0200

    enter the color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,21 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        creamy: {
+          100: "#eeebe7",
+          200: "#efd7c5",
+          300: "#e6c5aa",
+        },
+        green: {
+          100: "#d9e8d4",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
