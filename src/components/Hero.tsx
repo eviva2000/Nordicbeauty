c5c964bd68d86fb4b75cba3e332141d6975cf05f
eviva2000 <arjomandielaheh@gmail.com>
2025-08-08 18:39:52 +0200

    changed bg-image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,26 +13,26 @@ const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
 export default function Hero() {
   return (
     <section className="relative w-screen h-screen">
+      <div className="first-fold">
       <Navbar />
       <div className="main-content flex flex-col-reverse sm:flex-row px-6 justify-center items-center ">
-      <div>
-          <Image alt='flowers' src='/main-circle-with-flowers.svg' width={500} height={500}/>
-      </div>
+    
         <div className=" relative  xl:max-w-[800px] lg:px-20">
-          <h1 className="text-[36px] sm:text-[64px] md:text-[80px]  leading-[1]  text-[#2f4f4f] text-center sm:text-right">
+          <h1 className="text-[36px] sm:text-[64px] md:text-[80px]  leading-[1]  text-[#4f2c25]">
             ZibaNordic
           </h1>
-          <h4 className="text-[18px] sm:text-[28px] md:text-[25px] lg:text-[30px] mt-2 text-[#2f4f4f] text-center sm:text-right">
+          <h4 className="text-[18px] sm:text-[28px] md:text-[25px] lg:text-[30px] mt-2 ">
             منتخبی از برندهای اصیل جهانی
           </h4>
-          <p className="text-right text-[#2f4f4f] mt-12 mb-6   max-w-md">
+          <p className=" mt-12 mb-6   max-w-md">
             در زیبانوردیک، ما زیبایی را اتفاقی نمی‌دانیم. با دقت و وسواس، محصولات آرایشی و بهداشتی
             از معتبرترین برندهای جهانی را انتخاب می‌کنیم و مستقیماً از اروپا به ایران می‌رسانیم.
             اینجا، کیفیت، اصالت و سلامت در اولویت‌اند — برای کسانی که زیبایی و سلامتی را جدی می‌گیرند
           </p>
-          <Link className="flex justify-end" href='https://www.instagram.com/zibanordic/'><div className="border border-inherit border-solid p-4 rounded-full  mx-auto sm:mx-0 w-3xs  flex items-center justify-center sm:justify-end text-[#2f4f4f]"> ما را در اینستاگرام دنبال کنید <img className="instagram-logo" alt="" src='/New Project (1).svg'/></div> </Link>
+          <Link className=" bg-white   p-4 rounded-full  mx-auto w-3xs  flex items-center justify-center sm:justify-center" href='https://www.instagram.com/zibanordic/'>ما را در اینستاگرام دنبال کنید </Link>
         </div>
      
+      </div>
       </div>
         <div style={{ marginTop:'15rem'}}>
           <Slider slides={SLIDES} options={OPTIONS}>
@@ -41,7 +41,7 @@ export default function Hero() {
                 <Image
                   className="embla__slide__img"
                   src={`/test${index + 1}.jpg`}
-                  alt="Your alt text"
+                  alt="slider image"
                   width={500}
                   height={500}
                 />
@@ -52,7 +52,6 @@ export default function Hero() {
           
           </div>
 
-      <div className="hidden lg:block absolute lg:-top-60 lg:-left-60 2xl:-top-48 2xl:-left-48 bg-[#e6c5aa] w-[500px] h-[500px] rounded-full" />
 <footer style={{height:'20vh',background:'gray'}}> Placeholder for footer</footer>
     </section>
   );
