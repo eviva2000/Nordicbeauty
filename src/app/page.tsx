a4c9960f175996e8a3aee53b6c4ffff0b07eb6b3
Arash <arash8877@gmail.com>
2025-08-08 18:27:27 +0200

    refactor the slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
 import Hero from "@/components/Hero";
 import Slider from "@/components/Slider/Slider";
-import { EmblaOptionsType } from "embla-carousel";
-import Image from "next/image";
 
-const OPTIONS: EmblaOptionsType = { loop: true };
-const SLIDE_COUNT = 3;
-const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
 
 //------------------ Main Page Component------------------
 export default function Home() {
@@ -13,19 +8,7 @@ export default function Home() {
     <div className="">
       <main className="">
         <Hero />
-        <Slider slides={SLIDES} options={OPTIONS}>
-          {SLIDES.map((index) => (
-            <div className="embla__slide" key={index}>
-              <Image
-                className="embla__slide__img"
-                src={`/test${index + 1}.jpg`}
-                alt="Your alt text"
-                width={500}
-                height={500}
-              />
-            </div>
-          ))}
-        </Slider>
+        <Slider/>
         <footer style={{ height: "20vh", background: "gray" }}> Placeholder for footer</footer>
       </main>
     </div>
