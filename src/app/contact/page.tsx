e297451fda6224dae1a416e66ef13b6acc89e1d9
Arash <arash8877@gmail.com>
2025-08-10 21:42:34 +0200

    add icons

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,13 @@
-import Image from "next/image";
+import { FaWhatsapp, FaInstagram } from "react-icons/fa";
+import { MdOutlineEmail } from "react-icons/md";
+
 export default function Hero() {
   return (
     <section className="relative w-screen h-screen">
       <div className="first-fold">
+        <FaWhatsapp  className="w-40 h-40 text-[#4f2c25]"/>
+        <MdOutlineEmail className="w-40 h-40 text-[#4f2c25]"/>
+        <FaInstagram className="w-40 h-40 text-[#4f2c25]"/>
       </div>
     </section>
   );
