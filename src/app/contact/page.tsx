4e666438e9848193dcd6d7424fcac63a454677f7
Arash <arash8877@gmail.com>
2025-08-11 20:43:13 +0200

    modify contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,27 +4,27 @@ import { MdOutlineEmail } from "react-icons/md";
 export default function Hero() {
   return (
     <section className="relative w-screen h-screen">
-      <div className="first-fold px-8 md:px-12 xl:px-24">
-        <div className="translate-y-60 max-w-5xl bg-white m-auto border-4 rounded-[50px] p-12">
-          <h2 className="text-4xl font-bold text-center mb-6 md:mb-12 xl:mb-24">تماس با ما</h2>
+      <div className="first-fold px-8 md:px-24 lg:px-32">
+        <div className="translate-y-60 max-w-5xl bg-white m-auto border-4 rounded-[50px] p-12 pb-18">
+          <h2 className="text-xl xl:text-4xl font-bold text-center mb-6 md:mb-12 xl:mb-24">تماس با ما</h2>
           <div className="flex flex-col items-start text-left gap-2 md:gap-4 xl:gap-6">
             <div className="flex items-center gap-4 xl:gap-6 font-bold text-[20px] md:text-[30px] xl:text-[40px]">
               <FaWhatsapp className="w-12 md:w-16 xl:w-24 h-12 md:h-16 xl:h-24 text-[#4f2c25]" />
-              <p>WhatsApp: 093722331122</p>
+              <p className="text-sm md:text-xl xl:text-4xl">WhatsApp: 093722331122</p>
             </div>
             <a
               href="mailto:zibanordic@gmail.com"
               className="flex items-center gap-4 xl:gap-6 font-bold text-[20px] md:text-[30px] xl:text-[40px]"
             >
               <MdOutlineEmail className="w-12 md:w-16 xl:w-24 h-12 md:h-16 xl:h-24 text-[#4f2c25]" />
-              <p>zibanordic@gmail.com</p>
+              <p className="text-sm md:text-xl xl:text-4xl">zibanordic@gmail.com</p>
             </a>
             <a
               href="https://www.instagram.com/zibanordic/"
               className="flex items-center gap-4 xl:gap-6 font-bold text-[20px] md:text-[30px] xl:text-[40px]"
             >
               <FaInstagram className="w-12 md:w-16 xl:w-24 h-12 md:h-16 xl:h-24 text-[#4f2c25]" />
-              <p>@zibanordic</p>
+              <p className="text-sm md:text-xl xl:text-4xl">@zibanordic</p>
             </a>
           </div>
         </div>
