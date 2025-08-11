d244d7a3a6db8748c30d35f3895cb7a691baf2e4
Arash <arash8877@gmail.com>
2025-08-11 20:45:41 +0200

    give link to the logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
     <nav className="absolute top-0 left-0 z-10 w-full flex justify-end items-center px-4 md:px-20 py-4 md:py-6">
-      <div className="w-[50px] h-[50px] md:w-[75px] md:h-[75px] lg:w-[80px] lg:h-[80px] overflow-hidden rounded-full">
+      <Link href='/'  className="w-[50px] h-[50px] md:w-[75px] md:h-[75px] lg:w-[80px] lg:h-[80px] overflow-hidden rounded-full">
         <Image src="/logo.png" alt="logo" className="w-full h-full object-cover" width={500} height={500}/>
-      </div>
+      </Link>
     </nav>
   );
 }
