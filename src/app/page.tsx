d1d25609d4c13c89407aad6b702a0c790fb0889c
Arash <arash8877@gmail.com>
2025-08-18 10:29:15 +0200

    add instagram section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/Hero";
 import Slider from "@/components/Slider/Slider";
 import WhatWeDeliver from "@/components/WhatWeDeliver";
+import InstagramSection from "@/components/InstagramSection";
 
 //------------------ Main Page Component------------------
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
         <Hero />
         <WhatWeDeliver />
         <Slider />
+        <InstagramSection />
     </>
   );
 }
--- src/components/WhatWeDeliver.tsx
+++ src/components/WhatWeDeliver.tsx
@@ -1,24 +1,33 @@
-import {ShieldCheck,Gem,Sprout} from 'lucide-react'
+import { ShieldCheck, Gem, Sprout } from "lucide-react";
 
 export default function WhatWeDeliver() {
-
-    return (
-        <section className=" py-16 bg-[#2f4f4f] text-white">
-        <div className="container px-4 mx-auto">
-            <h2 className="text-3xl font-bold text-center mb-8">زیبایی یعنی حس خوب</h2>
-            <p className="text-lg text-center mb-4 max-w-2xl mx-auto">
-            زیبایی فقط در ظاهر نیست، در احساسی‌ست که هر روز با خودتان دارید.
-            ما در زیبانوردیک باور داریم که انتخاب محصولات درست می‌تواند آرامش، اعتمادبه‌نفس و درخشش طبیعی شما را زنده کند.
-            </p>
-            <p className='max-w-2xl mx-auto'>
-            هر محصولی که اینجا می‌بینید با عشق انتخاب شده تا بخشی از روتین روزانه‌ی شما شود و لحظه‌ای کوتاه از مراقبت شخصی را به تجربه‌ای لذت‌بخش تبدیل کند.
-            </p>
-            <ul className="list-inside text-lg flex gap-8 justify-center mt-8">
-            <li className='flex flex-col'> <ShieldCheck size={60} strokeWidth={1} /> اصالت </li>
-            <li  className='flex flex-col'> <Gem size={60} strokeWidth={1} /> کیفیت   </li>
-            <li  className='flex flex-col'> <Sprout size={60} strokeWidth={1} /> سلامت </li>
-            </ul>
-        </div>
-        </section>
-    );
+  return (
+    <section className=" py-16 bg-[#2f4f4f] text-white">
+      <div className="container px-4 mx-auto">
+        <h2 className="text-3xl font-bold text-center mb-8">زیبایی یعنی حس خوب</h2>
+        <p className="text-lg text-center mb-4 max-w-2xl mx-auto">
+          زیبایی فقط در ظاهر نیست، در احساسی‌ست که هر روز با خودتان دارید. ما در زیبانوردیک باور
+          داریم که انتخاب محصولات درست می‌تواند آرامش، اعتمادبه‌نفس و درخشش طبیعی شما را زنده کند
+        </p>
+        <p className="max-w-2xl mx-auto">
+          هر محصولی که اینجا می‌بینید با عشق انتخاب شده تا بخشی از روتین روزانه‌ی شما شود و لحظه‌ای
+          کوتاه از مراقبت شخصی را به تجربه‌ای لذت‌بخش تبدیل کند
+        </p>
+        <ul className="list-inside text-lg flex gap-8 justify-center mt-8">
+          <li className="flex flex-col">
+            {" "}
+            <ShieldCheck size={60} strokeWidth={1} /> اصالت{" "}
+          </li>
+          <li className="flex flex-col">
+            {" "}
+            <Gem size={60} strokeWidth={1} /> کیفیت{" "}
+          </li>
+          <li className="flex flex-col">
+            {" "}
+            <Sprout size={60} strokeWidth={1} /> سلامت{" "}
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 }
