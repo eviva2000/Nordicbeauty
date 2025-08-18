4ff0952d4cbad6cfce2b5937aa0c45e0244661d6
Arash <arash8877@gmail.com>
2025-08-18 10:37:47 +0200

    modify istagram section

--- src/components/InstagramSection.tsx
+++ src/components/InstagramSection.tsx
@@ -3,24 +3,22 @@ import { SiInstagram } from "react-icons/si";
 export default function InstagramSection() {
   return (
     <section className=" py-16 bg-[#f3f4f1] text-[#2f4f4f]">
-      <div className="container px-4 mx-auto">
-        <h2 className="text-3xl font-bold text-center mb-8">ما را در اینستاگرام دنبال کنید</h2>
-        <div className="flex flex-col items-center justify-center gap-8">
-          <p className="text-lg max-w-2xl">
-            برای آشنایی بیشتر با محصولات ما می‌توانید به صفحه اینستاگرام مراجعه کنید. در آنجا تمامی
-            اطلاعات مورد نیاز درباره محصولات، به همراه جدیدترین خبرها و به‌روزرسانی‌ها در دسترس شما
-            قرار دارد. ما تلاش می‌کنیم همواره تازه‌ترین و کامل‌ترین جزئیات را در اختیار شما بگذاریم
-            تا انتخابی آگاهانه و مطمئن داشته باشید
-          </p>
-          <a
-            href="https://www.instagram.com/zibanordic/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className=""
-          >
-            <SiInstagram className="w-16 xl:w-20 h-16 xl:h-20 text-[#2f4f4f] mx-auto mt-8" />
-          </a>
-        </div>
+      <div className="container mx-auto px-4 flex flex-col gap-8 items-center justify-center">
+        <h2 className="text-3xl font-bold text-center">ما را در اینستاگرام دنبال کنید</h2>
+        <p className="text-lg max-w-2xl">
+          برای آشنایی بیشتر با محصولات ما می‌توانید به صفحه اینستاگرام مراجعه کنید. در آنجا تمامی
+          اطلاعات مورد نیاز درباره محصولات، به همراه جدیدترین خبرها و به‌روزرسانی‌ها در دسترس شما
+          قرار دارد. ما تلاش می‌کنیم همواره تازه‌ترین و کامل‌ترین جزئیات را در اختیار شما بگذاریم تا
+          انتخابی آگاهانه و مطمئن داشته باشید
+        </p>
+        <a
+          href="https://www.instagram.com/zibanordic/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className=""
+        >
+          <SiInstagram className="w-16 xl:w-20 h-16 xl:h-20 text-[#2f4f4f] mx-auto" />
+        </a>
       </div>
     </section>
   );
